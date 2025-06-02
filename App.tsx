8d4051cfffcf255a8bcc8d9e975b0a737c2beb8f
Harjot Singh <harjot.singh@chicmicstudios.in>
2025-06-02 11:10:42 +0530

    feat: disable fontScaling

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react';
 
+import { TextInput, TextStyle } from 'react-native';
+
 import { preloadFonts } from '@utils/constants';
 import { preloadImages } from '@utils/images';
 
@@ -12,8 +14,23 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 SplashScreen.preventAutoHideAsync();
+interface ExtendedText extends Text {
+  defaultProps: {
+    allowFontScaling: boolean;
+    style?: TextStyle;
+  };
+}
 
+interface ExtendedTextInput extends TextInput {
+  defaultProps: {
+    allowFontScaling: boolean;
+  };
+}
 export default function App() {
+  (Text as unknown as ExtendedText).defaultProps = { allowFontScaling: false };
+  (TextInput as unknown as ExtendedTextInput).defaultProps = {
+    allowFontScaling: false,
+  };
   // Preload images and fonts before rendering the app
   // This ensures that the app is ready to display content without a loading screen
   useEffect(() => {
